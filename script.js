533f559ad09a20ed04661050c0e33f3603b9ca87
RaglandCodes <raglandasir@outlook.com>
2020-07-29 12:35:38 +0530

    🌈 Random accent colour
    - Also made contact section less verbose
    - added ceritifcate section

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("HI")
+const colours = ['#004D40', ' #212121', ' #BF360C', ' #1B5E20', '#1A237E'];
+let randomColor = colours[Math.floor(Math.random() * colours.length)];
+
+console.log(`${randomColor} <== randomColor\n\n`);
+
+let root = document.documentElement;
+root.style.setProperty('--accent-colour', randomColor);
